1734f39e732b720400a73e97c62c1c41550200e8
Romit Sutariya <romitsutariya@gamil.com>
2022-07-30 21:34:12 +0530

    add url shortnet code

---
+++ src/test/java/com/wlater/urlshortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlater.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
